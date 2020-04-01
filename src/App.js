4b7ce50757c34f61f839c743e10265e7a83edb7f
Manoj <manureddy.g94@gmail.com>
2020-04-01 12:47:30 +0530

    removing error

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. env is {process.env}
+          Edit <code>src/App.js</code> and save to reload. env is 
         </p>
         <a
           className="App-link"
