6f9ad8ee0e18d38ba94210fe2fd205df1951c2f9
Manoj <manureddy.g94@gmail.com>
2020-04-01 12:26:48 +0530

    integrate sentry with github

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as Sentry from '@sentry/browser';
+import * as serviceWorker from './serviceWorker';
+
+// Initialise SENTRY
+Sentry.init({dsn: "https://1ac979c08dc24df9b4b47a9ebf17e0ff@sentry.io/5184398"});
+
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
